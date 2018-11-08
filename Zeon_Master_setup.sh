acb42df4aaa7352f6fa848ec58b72d7c7ef9149d
Pubfred <zeonmymail@gmail.com>
2018-11-09 00:42:48 +0100

    Update Zeon_Master_setup.sh

--- Zeon_Master_setup.sh
+++ Zeon_Master_setup.sh
@@ -55,7 +55,7 @@ genkey=$1
 clear
 echo -e "${YELLOW}Zeon Masternode Setup Script${NC}"
 echo -e "${GREEN}Updating system and installing required packages...${NC}"
-sudo DEBIAN_FRONTEND=noninteractive apt-get -yq  update
+# sudo DEBIAN_FRONTEND=noninteractive apt-get -yq  update
 
 # sudo apt-get update -y
 # DEBIAN_FRONTEND=noninteractive
