9682f2c2988e093bfbb4673c727fd6b572aac50e
Pubfred <zeonmymail@gmail.com>
2018-11-10 00:02:05 +0100

    Update Zeon_Master_setup.sh

--- Zeon_Master_setup.sh
+++ Zeon_Master_setup.sh
@@ -114,6 +114,7 @@ if  [[ $(swapon -s | wc -l) -gt 1 ]] ; then
 else
     echo -e "${YELLOW}Creating 1GB disk swap file. \nThis may take a few minutes!${NC} \a"
     sudo  echo "export PATH=$PATH:/sbin" >> ~/.profile
+    sudo -s
     source ~/.profile
     sudo fallocate -l 1G /swapfile
     sudo chmod 600 /swapfile
