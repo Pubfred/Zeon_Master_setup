ded46588bea69b134e23c035ad96ff94f6b04a68
Pubfred <zeonmymail@gmail.com>
2018-11-09 23:02:36 +0100

    Update nodemon.sh

--- nodemon.sh
+++ nodemon.sh
@@ -8,7 +8,7 @@
 #Processing command line params
 if [ -z $1 ]; then dly=5; else dly=$1; fi   # Default refresh time is 5 sec
 
-datadir="/$USER/.zeon$2"   # Default datadir is /root/.zeon
+datadir="/$PWD/.zeon$2"   # Default datadir is /root/.zeon
 
 # Install jq if it's not present
 dpkg -s jq 2>/dev/null >/dev/null || sudo apt-get -y install jq
@@ -23,17 +23,17 @@ Outbound connections to other Zeon nodes [Zeon datadir: $datadir]
 Node IP               Ping    Rx/Tx     Since  Hdrs   Height  Time   Ban
 Address               (ms)   (KBytes)   Block  Syncd  Blocks  (min)  Score
 ==========================================================================='
-zeon-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
+sudo zeon-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
 \(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight) ,\(.synced_headers) ,\(.synced_blocks)  ,\
 \((now-.conntime)/60|floor) ,\(.banscore)\"' | column -t -s ',' &&
 echo '==========================================================================='
 uptime
 echo '==========================================================================='
-echo 'Masternode Status: \n# zeon-cli masternode status' && zeon-cli -datadir=$datadir masternode status
+echo 'Masternode Status: \n# zeon-cli masternode status' && sudo zeon-cli -datadir=$datadir masternode status
 echo '==========================================================================='
-echo 'Sync Status: \n# zeon-cli mnsync status' &&  zeon-cli -datadir=$datadir mnsync status
+echo 'Sync Status: \n# zeon-cli mnsync status' && sudo zeon-cli -datadir=$datadir mnsync status
 echo '==========================================================================='
-echo 'Masternode Information: \n# zeon-cli getinfo' && zeon-cli -datadir=$datadir getinfo
+echo 'Masternode Information: \n# zeon-cli getinfo' && sudo zeon-cli -datadir=$datadir getinfo
 echo '==========================================================================='
 echo 'Usage: nodemon.sh [refresh delay] [datadir index]'
 echo 'Example: nodemon.sh 10 22 will run every 10 seconds and query zeond in /$USER/.zeon22'
