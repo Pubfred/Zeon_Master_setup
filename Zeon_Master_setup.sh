014cc9061250fb26e5503205ac7299d4f654d590
Pubfred <zeonmymail@gmail.com>
2018-11-09 21:38:36 +0100

    Update Zeon_Master_setup.sh

--- Zeon_Master_setup.sh
+++ Zeon_Master_setup.sh
@@ -117,7 +117,7 @@ else
     sudo fallocate -l 1G /swapfile
     sudo chmod 600 /swapfile
     sudo mkswap /swapfile
-    sudo swapon /swapfile
+    sudo /sbin/swapon /swapfile
      
     
     if [ $? -eq 0 ]; then
